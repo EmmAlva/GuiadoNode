95a4dce0180dd6d2098a621486ef4566db009c50
EmmAlva <clioemma_28@hotmail.com>
2017-07-01 19:31:23 -0500

    to heroku

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ var app = express();
 app.use('/static', express.static(path.join(__dirname, 'node_modules')));//?????
 app.use('/static', express.static(path.join(__dirname, 'assets'))); //genera un path
 
+//setear el puerto
+  app.set('port', (process.env.PORT || 5000));
+
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.html');
-}).listen(8000);
+  
+})//.listen(8000);
+
+app.listen(app.get('port'), function(){
+          
+console.log('Node app is running on port', app.get('port'));
+           })
